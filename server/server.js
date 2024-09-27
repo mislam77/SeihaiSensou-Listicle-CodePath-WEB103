a80d517a2316991a137d90bbedc8c90cf506335e
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2024-09-27 16:43:38 -0400

    added data for servants and the route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import servantsRouter from './routes/servants.js'
 
 const app = express();
 
 app.use('/public', express.static('./public'));
 app.use('/scripts', express.static('./public/scripts'));
+app.use('/servants', servantsRouter)
 
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">Seihai Sensou!</h1>');
